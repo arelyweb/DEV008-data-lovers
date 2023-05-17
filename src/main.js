afad92c17d26ccec7dd39ca16825eb055015ec76
Luz Vazquez <vgluzmaria96@gmail.com>
2023-05-16 21:53:09 -0600

    Creacion del modal html y scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { example } from './data.js';
+import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
+
+/* -------------------------------------------------------------------------- */
+/*                              Interaccion Modal                             */
+/* -------------------------------------------------------------------------- */
+
+const modalPokemon = document.getElementById("modalPokemon");
+const btnCerrar = document.getElementById("btnCerrar");
+
+btnCerrar.onclick = function () {
+  modalPokemon.style.display = "none";
+};
+
+// window.onclick = function (event) {
+//   if (event.target === modalPokemon) {
+//     modalPokemon.style.display = "none";
+//   }
+// };
 
 console.log(example, data);
