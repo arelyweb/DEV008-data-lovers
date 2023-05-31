5cad8762c1db2a0da2210e8800cefd697cb76889
Luz Vazquez <vgluzmaria96@gmail.com>
2023-05-30 21:38:03 -0600

    modificaiones al test

--- test/const.js
+++ test/const.js
@@ -197,7 +197,7 @@ export const bulbasaur = {
   }
 }
 
-export const pokemonSinOrdenar = [
+export const pokemonSinOrdenarAsc = [
   {
     "num": "006",
     "name": "charizard",
@@ -317,6 +317,100 @@ export const pokemonSinOrdenar = [
     }
   },
   {
+    "num": "008",
+    "name": "wartortle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/008.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "22.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.07",
+      "base-capture-rate": "0.1"
+    },
+    "spawn-chance": "0.034",
+    "stats": {
+      "base-attack": "126",
+      "base-defense": "155",
+      "base-stamina": "153",
+      "max-cp": "1488",
+      "max-hp": "132"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "009",
+        "name": "blastoise",
+        "candy-cost": "100"
+      }],
+      "prev-evolution": [{
+        "num": "007",
+        "name": "squirtle",
+        "candy-cost": "25"
+      }]
+    }
+  },
+  {
     "num": "007",
     "name": "squirtle",
     "generation": {
@@ -406,6 +500,314 @@ export const pokemonSinOrdenar = [
           "num": "009",
           "name": "blastoise",
           "candy-cost": "100"
+        }]
+      }]
+    }
+  },
+  {
+    "num": "009",
+    "name": "blastoise",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/009.png",
+    "size": {
+      "height": "1.60 m",
+      "weight": "85.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.05",
+      "base-capture-rate": "0.05"
+    },
+    "spawn-chance": "0.0067",
+    "stats": {
+      "base-attack": "171",
+      "base-defense": "207",
+      "base-stamina": "188",
+      "max-cp": "2466",
+      "max-hp": "160"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "flash cannon",
+        "type": "steel",
+        "base-damage": "100",
+        "energy": "-100",
+        "move-duration-seg": "2.7"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "prev-evolution": [{
+        "num": "008",
+        "name": "wartortle",
+        "candy-cost": "100",
+        "prev-evolution": [{
+          "num": "007",
+          "name": "squirtle",
+          "candy-cost": "25"
+        }]
+      }]
+    }
+  },
+  {
+    "num": "008",
+    "name": "wartortle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/008.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "22.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.07",
+      "base-capture-rate": "0.1"
+    },
+    "spawn-chance": "0.034",
+    "stats": {
+      "base-attack": "126",
+      "base-defense": "155",
+      "base-stamina": "153",
+      "max-cp": "1488",
+      "max-hp": "132"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "009",
+        "name": "blastoise",
+        "candy-cost": "100"
+      }],
+      "prev-evolution": [{
+        "num": "007",
+        "name": "squirtle",
+        "candy-cost": "25"
+      }]
+    }
+  },]
+
+export const pokemonSinOrdenarDesc = [
+  {
+    "num": "006",
+    "name": "charizard",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/006.png",
+    "size": {
+      "height": "1.70 m",
+      "weight": "90.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire",
+      "flying"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.05",
+      "base-capture-rate": "0.05"
+    },
+    "spawn-chance": "0.0031",
+    "stats": {
+      "base-attack": "223",
+      "base-defense": "173",
+      "base-stamina": "186",
+      "max-cp": "2889",
+      "max-hp": "158"
+    },
+    "resistant": [
+      "fire",
+      "grass",
+      "fighting",
+      "bug",
+      "steel"
+    ],
+    "weaknesses": [
+      "water",
+      "electric",
+      "rock"
+    ],
+    "quick-move": [
+      {
+        "name": "fire spin",
+        "type": "fire",
+        "base-damage": "14",
+        "energy": "10",
+        "move-duration-seg": "1.1"
+      },
+      {
+        "name": "air slash",
+        "type": "flying",
+        "base-damage": "14",
+        "energy": "10",
+        "move-duration-seg": "1.2"
+      },
+      {
+        "name": "ember",
+        "type": "fire",
+        "base-damage": "10",
+        "energy": "10",
+        "move-duration-seg": "1"
+      },
+      {
+        "name": "wing attack",
+        "type": "flying",
+        "base-damage": "8",
+        "energy": "9",
+        "move-duration-seg": "0.8"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "fire blast",
+        "type": "fire",
+        "base-damage": "140",
+        "energy": "-100",
+        "move-duration-seg": "4.2"
+      },
+      {
+        "name": "dragon claw",
+        "type": "dragon",
+        "base-damage": "50",
+        "energy": "-33",
+        "move-duration-seg": "1.7"
+      },
+      {
+        "name": "overheat",
+        "type": "fire",
+        "base-damage": "160",
+        "energy": "-100",
+        "move-duration-seg": "4"
+      },
+      {
+        "name": "flamethrower",
+        "type": "fire",
+        "base-damage": "70",
+        "energy": "-50",
+        "move-duration-seg": "2.2"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "charmander candy",
+      "prev-evolution": [{
+        "num": "005",
+        "name": "charmeleon",
+        "candy-cost": "100",
+        "prev-evolution": [{
+          "num": "004",
+          "name": "charmander",
+          "candy-cost": "25"
         }]
       }]
     }
@@ -505,6 +907,100 @@ export const pokemonSinOrdenar = [
     }
   },
   {
+    "num": "007",
+    "name": "squirtle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/007.png",
+    "size": {
+      "height": "0.51 m",
+      "weight": "9.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.1",
+      "base-capture-rate": "0.2"
+    },
+    "spawn-chance": "0.58",
+    "stats": {
+      "base-attack": "94",
+      "base-defense": "121",
+      "base-stamina": "127",
+      "max-cp": "946",
+      "max-hp": "112"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "bubble",
+        "type": "water",
+        "base-damage": "12",
+        "energy": "14",
+        "move-duration-seg": "1.2"
+      },
+      {
+        "name": "tackle",
+        "type": "normal",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "aqua tail",
+        "type": "water",
+        "base-damage": "50",
+        "energy": "-33",
+        "move-duration-seg": "1.9"
+      },
+      {
+        "name": "water pulse",
+        "type": "water",
+        "base-damage": "70",
+        "energy": "-50",
+        "move-duration-seg": "3.2"
+      }
+    ],
+    "egg": "2 km",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "008",
+        "name": "wartortle",
+        "candy-cost": "25",
+        "next-evolution": [{
+          "num": "009",
+          "name": "blastoise",
+          "candy-cost": "100"
+        }]
+      }]
+    }
+  },
+  {
     "num": "009",
     "name": "blastoise",
     "generation": {
@@ -595,6 +1091,100 @@ export const pokemonSinOrdenar = [
           "name": "squirtle",
           "candy-cost": "25"
         }]
+      }]
+    }
+  },
+  {
+    "num": "008",
+    "name": "wartortle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/008.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "22.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.07",
+      "base-capture-rate": "0.1"
+    },
+    "spawn-chance": "0.034",
+    "stats": {
+      "base-attack": "126",
+      "base-defense": "155",
+      "base-stamina": "153",
+      "max-cp": "1488",
+      "max-hp": "132"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "009",
+        "name": "blastoise",
+        "candy-cost": "100"
+      }],
+      "prev-evolution": [{
+        "num": "007",
+        "name": "squirtle",
+        "candy-cost": "25"
       }]
     }
   },]
@@ -999,9 +1589,198 @@ export const pokemonesAsc = [
         "candy-cost": "25"
       }]
     }
-  },]
+  },
+  {
+    "num": "008",
+    "name": "wartortle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/008.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "22.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.07",
+      "base-capture-rate": "0.1"
+    },
+    "spawn-chance": "0.034",
+    "stats": {
+      "base-attack": "126",
+      "base-defense": "155",
+      "base-stamina": "153",
+      "max-cp": "1488",
+      "max-hp": "132"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "009",
+        "name": "blastoise",
+        "candy-cost": "100"
+      }],
+      "prev-evolution": [{
+        "num": "007",
+        "name": "squirtle",
+        "candy-cost": "25"
+      }]
+    }
+  },
+]
 
 export const pokemonesDesc = [
+  {
+    "num": "008",
+    "name": "wartortle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "about": "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+    "img": "https://www.serebii.net/pokemongo/pokemon/008.png",
+    "size": {
+      "height": "0.99 m",
+      "weight": "22.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "encounter": {
+      "base-flee-rate": "0.07",
+      "base-capture-rate": "0.1"
+    },
+    "spawn-chance": "0.034",
+    "stats": {
+      "base-attack": "126",
+      "base-defense": "155",
+      "base-stamina": "153",
+      "max-cp": "1488",
+      "max-hp": "132"
+    },
+    "resistant": [
+      "fire",
+      "water",
+      "ice",
+      "steel"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "quick-move": [
+      {
+        "name": "water gun",
+        "type": "water",
+        "base-damage": "5",
+        "energy": "5",
+        "move-duration-seg": "0.5"
+      },
+      {
+        "name": "bite",
+        "type": "dark",
+        "base-damage": "6",
+        "energy": "4",
+        "move-duration-seg": "0.5"
+      }
+    ],
+    "special-attack": [
+      {
+        "name": "aqua jet",
+        "type": "water",
+        "base-damage": "45",
+        "energy": "-33",
+        "move-duration-seg": "2.6"
+      },
+      {
+        "name": "ice beam",
+        "type": "ice",
+        "base-damage": "90",
+        "energy": "-50",
+        "move-duration-seg": "3.3"
+      },
+      {
+        "name": "hydro pump",
+        "type": "water",
+        "base-damage": "130",
+        "energy": "-100",
+        "move-duration-seg": "3.3"
+      }
+    ],
+    "egg": "not in eggs",
+    "buddy-distance-km": "3",
+    "evolution": {
+      "candy": "squirtle candy",
+      "next-evolution": [{
+        "num": "009",
+        "name": "blastoise",
+        "candy-cost": "100"
+      }],
+      "prev-evolution": [{
+        "num": "007",
+        "name": "squirtle",
+        "candy-cost": "25"
+      }]
+    }
+  },
   {
     "num": "008",
     "name": "wartortle",
