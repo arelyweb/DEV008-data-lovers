b8d434482a98d6694e369f0a926f83bba6f0dce6
Arely Gonzalez <arely.web@gmail.com>
2023-06-06 11:36:46 -0600

    Updates

---
+++ data.js
@@ -0,0 +1,93 @@
+//import pokemon from "./data/pokemon/pokemon.js";
+import data from "./data/pokemon/pokemon.js";
+
+/* ----------------- busca un pokemon de acuerdo a su nombre ---------------- */
+export const obtenerPokemon = (nombre) => {
+  return data.pokemon.find((pokemon) => pokemon.name === nombre);
+};
+
+
+/* ------------------ Ordena de forma ascendente y descente ----------------- */
+export const ordenarPokemon = (tipoOrdenamiento, arrayPokemones) => {
+  if (tipoOrdenamiento === "asc") {
+    return arrayPokemones.sort((pokemon1, pokemon2) => {
+      if (pokemon1.name > pokemon2.name) {
+        return 1;// coloca al pokemon 2 primero
+      }
+      if (pokemon1.name < pokemon2.name) {
+        return -1;  //pokemon 1 va primero
+      }
+      return 0;
+    });
+  } else {
+    return arrayPokemones.sort((pokemon1, pokemon2) => {
+      if (pokemon1.name > pokemon2.name) {
+        console.log(pokemon1.name, '>', pokemon2.name);
+        console.log('opk1');
+
+        return -1; //pokemon 1 va primero
+      }
+      if (pokemon1.name < pokemon2.name) {
+        console.log(pokemon1.name, '>', pokemon2.name);
+
+        console.log('opk2');
+        return 1;//pokemon 2 primero
+      }
+      return 0;
+    });
+  }
+};
+
+/* ------------------- Obtiene el total y tipos de pokemon ------------------ */
+export const contarTipoPokemon = () => {
+  const arrayTipoPokemon = [];
+
+  data.pokemon.forEach((pokemon) => {
+    pokemon.type.forEach((type) => {
+      const posicion = arrayTipoPokemon.findIndex((item) => item.tipoPokemon === type);
+      if (posicion === -1) {
+        arrayTipoPokemon.push({ tipoPokemon: type, total: 1 });
+      } else {
+        arrayTipoPokemon[posicion].total += 1;
+      }
+    });
+  });
+
+  return arrayTipoPokemon;
+
+};
+
+/* ------------------- Aplica SLICE al array que se mande ------------------- */
+export const cortarArrayPokemones = (inicio, fin, arrayPokemones) => {
+  return arrayPokemones.slice(inicio, fin);
+};
+
+/* -------- Calcula cuaantas paginas se necesitan para la paginacion -------- */
+export const calcularPaginas = (tamañoArreglo, pokemonesPorPagina) => {
+  return Math.ceil(tamañoArreglo / pokemonesPorPagina);
+};
+
+/* ---------------- Filtra los pokemones por nombre o numero ---------------- */
+export const filterData = (nombreNumero) => {
+  const sinEsp = nombreNumero.replace(/\s/g, '');
+  const sinCreS = sinEsp.replace(/[^a-zA-Z0-9 ]/g, '');
+  const pattern = sinCreS
+    .split("")
+    .map((x) => {
+      return `(?=.*${x})`; //?= pregunta . cualquier caracter excepto salto de linea * coincide con cero o mas instancias de un caracter (?=.*p)(?=.*i)(?=.*k)(?=.*a)
+    })
+    .join("");
+  const regex = new RegExp(`${pattern}`, "g");
+
+  const arrayPokemonBusquedaxNombre = data.pokemon.filter(
+    (pokemon) => pokemon.num.match(regex) || pokemon.name.match(regex)
+  ); //busca por nombre o numero
+  return arrayPokemonBusquedaxNombre;
+};
+
+/* -------------------------------------------------------------------------- */
+/*                            C A P I T A L I Z A R                           */
+/* -------------------------------------------------------------------------- */
+export const capitalizar = (palabra) => {
+  return palabra.charAt(0).toUpperCase() + palabra.slice(1);
+}
---
+++ main.js
@@ -0,0 +1,286 @@
+import {
+  filterData,
+  obtenerPokemon,
+  ordenarPokemon,
+  contarTipoPokemon,
+  calcularPaginas,
+  cortarArrayPokemones,
+  capitalizar,
+} from "./data.js";
+// import data from './data/lol/lol.js';
+import data from "./data/pokemon/pokemon.js";
+
+/* -------------------------------------------------------------------------- */
+/*                             Variables globales                             */
+/* -------------------------------------------------------------------------- */
+const modalPokemon = document.getElementById("modalPokemon");
+let pokemones = [];
+const pokemonesPorPagina = 12;
+let paginaActual;
+let totalPaginas;
+
+
+/* ----------------------------- Carga de pagina ---------------------------- */
+window.addEventListener("load", function () {
+  crearPaginacionInicial(data.pokemon);
+});
+
+/* ---------------------------- Reseteo de pagina --------------------------- */
+document.getElementById("pokemon").addEventListener("click", function () {
+  crearPaginacionInicial(data.pokemon);
+  document.getElementById("idInputBusqueda").value = "";
+});
+
+/* --------------------- Busca los pókemones por nombre --------------------- */
+document.getElementById("idBotonBuscar").addEventListener("click", function () {
+  const inputBusquedaNombre = document.getElementById("idInputBusqueda").value;
+  const pokemonesFiltrados = filterData(inputBusquedaNombre.toLowerCase());
+  pokemones = pokemonesFiltrados;
+  crearPaginacionInicial(pokemones);
+});
+
+function muestraDatosTabla(arrayPokemones) {
+  const cuerpoTabla = document.querySelector("#cuerpoTabla");
+  cuerpoTabla.innerHTML = ""; //Se limpia la tabla
+  arrayPokemones.forEach((pokemon) => {
+    // Crear un <tr>
+    const tr = document.createElement("tr");
+    // Creamos un <td> de numero
+    const tdNumero = document.createElement("td");
+    tdNumero.textContent = pokemon.num; // el textContent del td es el nombre
+    tr.appendChild(tdNumero);
+    // Creamos el <td> de nombre
+    const tdNombre = document.createElement("td");
+    tdNombre.textContent = pokemon.name; // el textContent del td es el nombre
+    tr.appendChild(tdNombre);
+
+    // El td de generacion
+    const tdGeneracion = document.createElement("td");
+    tdGeneracion.textContent = pokemon.generation.name;
+    tr.appendChild(tdGeneracion);
+    // El td del tipo
+    const tdTipo = document.createElement("td");
+    tdTipo.textContent = pokemon.type;
+    tr.appendChild(tdTipo);
+
+    // Crea el <td> en donde contiene el boton de ver
+    const tdVer = document.createElement("td");
+
+    // Crea el <botton> en donde contiene el boton de ver
+    const btnModal = document.createElement("button");
+    btnModal.classList.add("buttonpill");
+    btnModal.innerHTML = "Ver pokemón";
+    btnModal.value = pokemon.name;
+
+    //agregar evento click al boton
+    btnModal.addEventListener("click", mostrarModal);
+
+    //Agrega el boton al <td>
+    tdVer.appendChild(btnModal);
+    //Agrega el <td> al elemento tr
+    tr.appendChild(tdVer);
+    //Agrega el tr a la tabla
+    cuerpoTabla.appendChild(tr);
+
+    // Y el ciclo se repite hasta que se termina de recorrer todo el arreglo
+  });
+}
+
+/* ------------------- Muestra/Oculta grafica de pokemones ------------------ */
+
+document.getElementById("promedioPokemones").addEventListener("click", function (e) {
+
+  if (e.target.innerHTML === "Ver estadística") {
+    document.getElementById("promedioPokemones").innerHTML = "Ver tabla";
+    document.getElementById("idtablapokemones").style.display = "none";
+    document.getElementById("divBusqueda").style.display = "none";
+    document.getElementById("divOrden").style.display = "none";
+    document.getElementById("divPaginacion").style.display = "none";
+    document.getElementById("estadistica").style.display = "flex";
+
+    const estadistica = contarTipoPokemon();
+
+    const conteoPokemon = estadistica.sort(function (a, b) {
+      return a.total - b.total;
+    });
+    document.getElementById("tipoPokemonMayor").innerHTML = `Tipo: ${capitalizar(conteoPokemon[conteoPokemon.length - 1].tipoPokemon)}`
+    document.getElementById("totalPokemonMayor").innerHTML = conteoPokemon[conteoPokemon.length - 1].total
+    document.getElementById("tipoPokemonMenor").innerHTML = `Tipo: ${capitalizar(conteoPokemon[0].tipoPokemon)}`
+    document.getElementById("totalPokemonMenor").innerHTML = conteoPokemon[0].total
+
+
+    const divGrafica = document.getElementById("grafica");
+    divGrafica.innerHTML = "";
+    divGrafica.style.display = "flex";
+    const ctx = document.createElement("canvas");
+    divGrafica.appendChild(ctx);
+    const tipoPokemones = estadistica.map((item) => item.tipoPokemon);
+    const totalPokemones = estadistica.map((item) => item.total);
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: tipoPokemones,
+        datasets: [
+          {
+            label: "Total de pokemones",
+            data: totalPokemones,
+            borderWidth: 1,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(201, 203, 207, 0.2)",
+            ],
+            borderColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)",
+            ],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  } else {
+    document.getElementById("promedioPokemones").innerHTML =
+      "Ver estadística";
+    document.getElementById("grafica").style.display = "none";
+    document.getElementById("idtablapokemones").style.display = "table";
+    document.getElementById("divPaginacion").style.display = "flex";
+    document.getElementById("estadistica").style.display = "none";
+    document.getElementById("divOrden").style.display = "block";
+    document.getElementById("divBusqueda").style.display = "flex";
+    document.getElementById("idtablapokemones").classList.add("main-conteiner__table");
+  }
+});
+
+/* ------------------ Ordenamiento ascendente y descendente ----------------- */
+document.getElementById("slcOrdenar").addEventListener("change", function (event) {
+  const tipoOrdenamiento = event.target.value;
+  const pokemonOrdenados = ordenarPokemon(tipoOrdenamiento, pokemones);
+  crearPaginacionInicial(pokemonOrdenados);
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                  M O D A L                                 */
+/* -------------------------------------------------------------------------- */
+
+/* ---------------------------- Muestra el modal ---------------------------- */
+function mostrarModal(event) {
+  const informacion = document.getElementById("informacion");
+  informacion.innerHTML = "";
+  modalPokemon.style.display = "block";
+  const nombrePokemon = event.target.value;
+  const pokemonEncontrado = obtenerPokemon(nombrePokemon);
+
+  document.getElementById("nombrePokemon").innerHTML = pokemonEncontrado.name.toUpperCase();
+  document.getElementById("nombrePokemontwo").innerHTML = pokemonEncontrado.name.toUpperCase();
+  informacion.appendChild(document.createElement("p")).innerHTML = `Acerca de: ${capitalizar(pokemonEncontrado.about)}`;
+  informacion.appendChild(document.createElement("p")).innerHTML = `Tipo: ${capitalizar(pokemonEncontrado.type.join(", "))}`;
+  informacion.appendChild(document.createElement("p")).innerHTML = `Movimientos rápidos: ${pokemonEncontrado["quick-move"].map(function (move) { return capitalizar(move.name) })}`;
+  informacion.appendChild(document.createElement("p")).innerHTML = `Ataque especial: ${pokemonEncontrado["special-attack"].map(function (move) { return capitalizar(move.name) })}`;
+  informacion.appendChild(document.createElement("p")).innerHTML = `Debilidades: ${capitalizar(pokemonEncontrado.weaknesses.join(", "))}`;
+  informacion.appendChild(document.createElement("p")).innerHTML = `Resistencia: ${capitalizar(pokemonEncontrado.resistant.join(", "))}`;
+
+  document.getElementById("imagenPokemon").src = pokemonEncontrado.img;
+}
+/* ----------------------- Botones de cierre del modal ---------------------- */
+
+document.getElementById("btnCerrar").onclick = function () {
+  modalPokemon.style.display = "none";
+};
+
+document.getElementById("modalPokemon").onclick = function (e) {
+  if (e.target.id === "modalPokemon") {
+    modalPokemon.style.display = "none";
+  }
+};
+
+// document.getElementById("elipseCerrar").onclick = function () {
+//   modalPokemon.style.display = "none";
+// };
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                             P A G I N A C I O N                            */
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------- Crea la paginacion inicial ----------------------- */
+function crearPaginacionInicial(pokemonesAPaginar) {
+  pokemones = pokemonesAPaginar; // Es el arreglo que se cargara
+  paginaActual = 1; // Cuando se crea la paginación  la pagina por defecto es 1
+  totalPaginas = calcularPaginas(pokemones.length, pokemonesPorPagina); //Calculamos cuantos botoncitos de paginacion vamos a necesitar
+  crearBotonesPaginacion(); // Funcion que crea los botones con numeros
+  const pokemonesAMostrar = cortarArrayPokemones(pokemonesPorPagina * (paginaActual - 1), pokemonesPorPagina * paginaActual, pokemones);//Vamos por los primeros pokemones
+  muestraDatosTabla(pokemonesAMostrar); //Mostramos los pokemones en la tabla
+  //Pintamos el boton
+  pintarCuadrito()
+}
+
+/* --------------------- Evento clic del boton Adelante --------------------- */
+document.getElementById("btnAdelante").addEventListener("click", () => {
+  if (paginaActual < totalPaginas) {
+    paginaActual = paginaActual + 1;
+    const pokemonesAMostrar = cortarArrayPokemones(pokemonesPorPagina * (paginaActual - 1), pokemonesPorPagina * paginaActual, pokemones);
+    muestraDatosTabla(pokemonesAMostrar);
+    //Pintamos el boton
+    pintarCuadrito()
+  }
+});
+
+/* ----------------------- Evento clic del boton Atras ---------------------- */
+document.getElementById("btnAtras").addEventListener("click", () => {
+  if (paginaActual > 1) {
+    paginaActual = paginaActual - 1;
+    const pokemonesAMostrar = cortarArrayPokemones(pokemonesPorPagina * (paginaActual - 1), pokemonesPorPagina * (paginaActual), pokemones);
+    muestraDatosTabla(pokemonesAMostrar);
+    //Pintamos el boton
+    pintarCuadrito()
+  }
+});
+
+/* --------------- Crea los botones de las páginas disponibles -------------- */
+function crearBotonesPaginacion() {
+  const contenedorBotones = document.getElementById("contenedorBotones");
+  contenedorBotones.innerHTML = "";
+  for (let i = 1; i <= totalPaginas; i++) {
+    const boton = document.createElement("button");
+    boton.innerHTML = i;
+    boton.id = 'btn' + boton.innerHTML
+    boton.classList.add("buttonPagination")
+    boton.addEventListener("click", cargarPokemonesPorPagina);
+    contenedorBotones.appendChild(boton);
+  }
+}
+
+/* ------ Funcion de cada uno de los botones de las paginas disponibles ----- */
+function cargarPokemonesPorPagina(e) {
+  paginaActual = parseInt(e.target.innerHTML);
+  const pokemonesAMostrar = cortarArrayPokemones(pokemonesPorPagina * (paginaActual - 1), pokemonesPorPagina * paginaActual, pokemones);
+  muestraDatosTabla(pokemonesAMostrar);
+  pintarCuadrito();
+}
+
+function pintarCuadrito() {
+  for (let i = 1; i <= totalPaginas; i++) {
+    document.getElementById(`btn${i}`).classList.remove("btnActual")
+  }
+  document.getElementById(`btn${paginaActual}`).classList.add("btnActual")
+
+}
+
+
